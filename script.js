0218266fb310ac7019e92a24c7d805cdae890b3d
Ygor <ybirochi123456@gmail.com>
2025-02-20 21:28:36 -0300

    Dark mode *arrumar o site

--- script.js
+++ script.js
@@ -17,6 +17,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 800)
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggles = document.querySelectorAll('[data-theme-toggle]');
+    const body = document.body;
+
+    function toggleDarkMode() {
+        body.classList.toggle('dark-mode');
+        const isDarkMode = body.classList.contains('dark-mode');
+        localStorage.setItem('dark-mode', isDarkMode ? 'enabled' : 'disabled');
+    }
+
+    themeToggles.forEach(btn => {
+        btn.addEventListener('click', toggleDarkMode);
+    });
+
+    // Verificar o estado salvo
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        body.classList.add('dark-mode');
+    }
+
+    // Atualizar ícones
+    function updateIcons() {
+        themeToggles.forEach(icon => {
+            icon.style.display = body.classList.contains('dark-mode') ? 
+                (icon.classList.contains('fa-moon') ? 'none' : 'block') :
+                (icon.classList.contains('fa-sun') ? 'none' : 'block');
+        });
+    }
+    updateIcons();
+    body.addEventListener('theme-change', updateIcons);
+});
 window.addEventListener("scroll", function(){
     const header = document.getElementById("header");
     const scrollPosition = window.scrollY;
